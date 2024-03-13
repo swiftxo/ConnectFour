7944d1f43fe7db2669a7992be739113ad29e0558
swiftxo <146109179+swiftxo@users.noreply.github.com>
2024-03-13 16:20:51 -0400

    complete connect 4

--- connect4_gui.py
+++ connect4_gui.py
@@ -1,23 +1,40 @@
 import pygame
+import os 
 from connect4_functions import NUM_COL,NUM_ROW
 
 SQUARESIZE = 100
 BLACK = (0,0,0)
 PURPLE = (25,8,86)
+RED = (255,0,0)
+YELLOW = (255,255,0)
 width = NUM_COL * SQUARESIZE
 height = (NUM_ROW+1) * SQUARESIZE
 size = (width,height)
 RADIUS = int(SQUARESIZE/2 - 6)
 
+
+
 def init_gui():
     pygame.init()
     screen = pygame.display.set_mode(size)
-    return screen
+    font_size = 75
+    font_name = "connect4_font.otf"
+    font = pygame.font.Font(font_name,font_size)
+    return screen,font
 
 def draw_board(board, screen):
-    for i in range(NUM_COL):
-        for j in range(NUM_ROW):
-            pygame.draw.rect(screen, PURPLE, (i*SQUARESIZE,j*SQUARESIZE+SQUARESIZE,SQUARESIZE,SQUARESIZE))
-            pygame.draw.circle(screen,BLACK, (i*SQUARESIZE+SQUARESIZE/2, j*SQUARESIZE+SQUARESIZE+SQUARESIZE/2) , RADIUS)
+    for c in range(NUM_COL):
+        for r in range(NUM_ROW):
+            pygame.draw.rect(screen, PURPLE, (c*SQUARESIZE, height - (r+1)*SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(height - (r*SQUARESIZE+SQUARESIZE/2))), RADIUS)
+
+    for c in range(NUM_COL):
+        for r in range(NUM_ROW):
+            if board[r][c] == 1:
+                pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), int(height - (r*SQUARESIZE+SQUARESIZE/2))), RADIUS)
+            elif board[r][c] == 2:
+                pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), int(height - (r*SQUARESIZE+SQUARESIZE/2))), RADIUS)
+    pygame.display.update()
+
 
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,11 @@
-print(board)
+def draw_board(board, screen):
+    for i in range(NUM_COL):
+        for j in range(NUM_ROW):
+            pygame.draw.rect(screen, PURPLE, (i*SQUARESIZE,j*SQUARESIZE+SQUARESIZE,SQUARESIZE,SQUARESIZE))
+            if board[j][i] == 0:
+                pygame.draw.circle(screen,BLACK, (i*SQUARESIZE+SQUARESIZE/2, j*SQUARESIZE+SQUARESIZE+SQUARESIZE/2) , RADIUS)
+            elif board[j][i] == 1:
+                pygame.draw.circle(screen,RED, (i*SQUARESIZE+SQUARESIZE/2, j*SQUARESIZE+SQUARESIZE+SQUARESIZE/2) , RADIUS)
+            elif board[j][i] == 2:
+                pygame.draw.circle(screen,YELLOW, (i*SQUARESIZE+SQUARESIZE/2, j*SQUARESIZE+SQUARESIZE+SQUARESIZE/2) , RADIUS)
+            pygame.display.update()
